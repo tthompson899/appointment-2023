d948fc4c5a17fdeddd7e284fdf418d9c03f0ee1c
Tiffany Thompson <tdthompson899@gmail.com>
2024-03-04 14:54:32 -0600

    Add test case for sending reminder email. Remove unused code.

--- app/Console/Commands/AppointmentReminderNotification.php
+++ app/Console/Commands/AppointmentReminderNotification.php
@@ -3,11 +3,8 @@
 namespace App\Console\Commands;
 
 use App\Models\Appointment;
-use App\Models\Type;
-use App\Models\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\AppointmentReminder;
 
@@ -44,14 +41,9 @@ class AppointmentReminderNotification extends Command
             ->where('date_of_appointment', '<=', $currentDateTime->copy()->addDays(7)->endOfDay())
             ->get();
 
-        // $upcomingAppointments = Appointment::factory()->for(User::factory()->create())
-        // ->for(Type::factory()->create())
-        // ->create([
-        //     'date_of_appointment' => $currentDateTime,
-        // ]);
-
         info("Appointment Reminder: " . $currentDateTime->copy()->addDays(7)->endOfDay());
-
+        info("Count_of_appointments_found: " . $upcomingAppointments->count());
+        
         if ($upcomingAppointments->count() > 0) {
             foreach($upcomingAppointments as $appointment) {
                 Mail::to($appointment->user->email)->send(new AppointmentReminder($appointment));
